a99999ba68d97b6c742cc3da7b4b842c93bb2e24
Leonardo <leonardomullerdelima@pm.me>
2025-07-18 18:59:00 -0300

    feat: implement insert function

--- bst.js
+++ bst.js
@@ -8,12 +8,28 @@ const Tree = (initialArray = []) => {
     if (start > end - 1) return null;
 
     const mid = Math.floor(end / 2);
-
     const root = Node(arr[mid]);
     root.left = buildTree(arr.slice(0, mid));
     root.right = buildTree(arr.slice(mid + 1, end));
 
     return root;
+  }
+
+  function insert(value, node = treeRoot) {
+    if (value === node.data) return;
+    if (value < node.data) {
+      if (!node.left) {
+        node.left = Node(value);
+        return;
+      }
+      insert(value, node.left);
+    } else {
+      if (!node.right) {
+        node.right = Node(value);
+        return;
+      }
+      insert(value, node.right);
+    }
   }
 
   function prepare(arr) {
@@ -43,6 +59,8 @@ const Tree = (initialArray = []) => {
 
   return {
     treeRoot,
+    insert,
+    deleteItem,
     prettyPrint,
   };
 };
